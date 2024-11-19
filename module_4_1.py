77ee7484eafc871965419c27fdf22d3fcc3410e0
Matuha16 <matveevkarton03@gmail.com>
2024-11-19 14:02:14 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import devide as dev1
+from true_math import devide as devinf
+
+result1 = dev1(69, 3)
+result2 = dev1(3, 0)
+result3 = devinf(49, 7)
+result4 = devinf(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
